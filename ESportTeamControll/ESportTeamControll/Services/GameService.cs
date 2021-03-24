4fb4bc070b580b82b87466397b5c0ad93d792359
MarceloTonhetti <marcelotjunior2507@gmail.com>
2021-03-24 18:05:05 -0300

    Implementando Games

---
+++ ESportTeamControll/ESportTeamControll/Services/GameService.cs
@@ -0,0 +1,49 @@
+﻿using ESportTeamControll.Dal;
+using ESportTeamControll.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESportTeamControll.Services
+{
+	public class GameService : IGameService
+	{
+		private ESportTeamContext db = new ESportTeamContext();
+
+		public IEnumerable<Game> GetAll()
+		{
+			return db.Games.ToArray();
+		}
+
+		public Game GetById(int id)
+		{
+			return db.Games.First( g => g.Id == id);
+		}
+
+		public void Create(Game game)
+		{
+			db.Games.Add(game);
+			SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var game = GetById(id);
+			db.Games.Remove(game);
+			SaveChanges();
+		}
+
+		public void Edit(Game game)
+		{
+			var gameUpdate = GetById(game.Id);
+			gameUpdate.Name = game.Name;
+			SaveChanges();
+		}
+
+		public void SaveChanges()
+		{
+			db.SaveChanges();
+		}
+	}
+}
---
+++ ESportTeamControll/ESportTeamControll/Services/IGameService.cs
@@ -0,0 +1,24 @@
+﻿using ESportTeamControll.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESportTeamControll.Services
+{
+	public interface IGameService
+	{
+		IEnumerable<Game> GetAll();
+
+		Game GetById(int id);
+
+		void Create(Game game);
+
+		void Edit(Game game);
+
+		void Delete(int id);
+
+		void SaveChanges();
+
+	}
+}
